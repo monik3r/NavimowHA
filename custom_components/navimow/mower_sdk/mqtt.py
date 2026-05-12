8814df5f1022bab1ee6aee5a397ae88226153331
moniker <aaaaa@example.tld>
2026-05-11 23:03:04 -0700

    This correct?

--- custom_components/navimow/mower_sdk/mqtt.py
+++ custom_components/navimow/mower_sdk/mqtt.py
@@ -672,7 +672,11 @@ class NavimowMQTT:
             "NavimowMQTT subscribing cloud topics for %d device(s)", len(device_ids)
         )
         for device_id in device_ids:
-            self.client.subscribe(f"/downlink/vehicle/{device_id}/realtimeDate/location")
+            result, mid = await self.client.subscribe(f"/downlink/vehicle/{device_id}/realtimeDate/location")
+            _LOGGER.debug("MQTT subscribe location: result=%s mid=%s",
+                result, mid
+            )
+
             self.client.subscribe(f"/downlink/vehicle/{device_id}/realtimeDate/state")
             self.client.subscribe(f"/downlink/vehicle/{device_id}/realtimeDate/event")
             self.client.subscribe(
