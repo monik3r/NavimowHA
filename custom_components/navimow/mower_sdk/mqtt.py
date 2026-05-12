ef6233fd074ba41c8565c5bfa110ddfeff8e6e70
moniker <aaaaa@example.tld>
2026-05-11 23:31:26 -0700

    break one topic, change logging, see if we have some weird caching

--- custom_components/navimow/mower_sdk/mqtt.py
+++ custom_components/navimow/mower_sdk/mqtt.py
@@ -669,13 +669,13 @@ class NavimowMQTT:
             return
 
         _LOGGER.info(
-            "NavimowMQTT subscribing cloud topics for %d device(s)", len(device_ids)
+            "NavimowMQTT asd subscribing cloud topics for %d device(s)", len(device_ids)
         )
         for device_id in device_ids:
             self.client.subscribe(f"/downlink/vehicle/{device_id}/realtimeDate/state")
             self.client.subscribe(f"/downlink/vehicle/{device_id}/realtimeDate/event")
             self.client.subscribe(
-                f"/downlink/vehicle/{device_id}/realtimeDate/attributes"
+                f"/downlink/vehicle/{device_id}/realtimeDate/#"
             )
             self.client.subscribe(f"/downlink/vehicle/{device_id}/realtimeDate/#")
 
